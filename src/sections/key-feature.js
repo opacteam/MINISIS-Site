91ded80c507e213ed125c59729f4d9354b58ea0d
opacteam <mike@minisisinc.com>
2022-01-04 22:36:13 +0700

    finished key featured

--- src/sections/key-feature.js
+++ src/sections/key-feature.js
@@ -1,73 +1,90 @@
 /** @jsx jsx */
-import { jsx } from 'theme-ui';
-import { Container, Grid } from 'theme-ui';
-import SectionHeader from '../components/section-header';
-import FeatureCardColumn from 'components/feature-card-column.js';
-import Performance from 'assets/key-feature/performance.svg';
-import Partnership from 'assets/key-feature/partnership.svg';
-import Subscription from 'assets/key-feature/subscription.svg';
-import Support from 'assets/key-feature/support.svg';
+import { jsx } from "theme-ui";
+import { Container, Grid } from "theme-ui";
+import SectionHeader from "../components/section-header";
+import FeatureCardColumn from "components/feature-card-column.js";
+import Secure from "assets/key-feature/Secure.png";
+import Cloud from "assets/key-feature/Cloud.png";
+import Complete from "assets/key-feature/Complete.png";
+import Design from "assets/key-feature/Design.png";
+import Help from "assets/key-feature/Help.png";
+import FeatureCard from "components/feature-card";
 
 const data = [
   {
     id: 1,
-    imgSrc: Performance,
-    altText: 'Fast Performance',
-    title: 'Fast Performance',
+    imgSrc: Secure,
+    altText: "Secure & Customizable",
+    title: "Secure & Customizable",
     text:
-      'Get your blood tests delivered at home collect a sample from the your blood tests.',
+      "All of our software runs on the MINISIS core database engine which uses a unique markup language. This keeps it safe from hackers targeting more common languages, and being able to create custom solutions."
   },
   {
     id: 2,
-    imgSrc: Partnership,
-    altText: 'Partnership deal',
-    title: 'Partnership deal',
+    imgSrc: Cloud,
+    altText: "Cloud-based Hosting & Web Solutions",
+    title: "Cloud-based Hosting & Web Solutions",
     text:
-      'Get your blood tests delivered at home collect a sample from the your blood tests.',
+      "Our hosted solutions on the MS Azure Platform allow for redundant system availability without any need for local management. Enjoy the freedom to focus on your daily work activities and not IM/IT."
   },
   {
     id: 3,
-    imgSrc: Subscription,
-    altText: 'Pro Subscription',
-    title: 'Pro Subscription',
+    imgSrc: Design,
+    altText: "Full Design Services",
+    title: "Full Design Services",
     text:
-      'Get your blood tests delivered at home collect a sample from the your blood tests.',
+      "We are familiar with guiding clients through the process of bringing a collection online as well as integrating into an existing website. This allows us to keep costs low and deliver projects quickly."
   },
   {
     id: 4,
-    imgSrc: Support,
-    altText: 'Customer Support',
-    title: 'Customer Support',
+    imgSrc: Help,
+    altText: "Personal Help & Easy Tutorials",
+    title: "Personal Help & Easy Tutorials",
     text:
-      'Get your blood tests delivered at home collect a sample from the your blood tests.',
-  },
+      "As a MINISIS client you will have access to our log in area where you can find video tutorials, industry resources, events, and a full client support system. "
+  }
 ];
 
 export default function KeyFeature() {
   return (
-   <h1>Key Feature</h1>
+    <section sx={{ variant: "section.keyFeature" }} id="features">
+      <Container>
+        <SectionHeader slogan="Test Slogan" title="Test Title"></SectionHeader>
+        <Grid sx={styles.grid}>
+          {data.map(item => (
+            <FeatureCardColumn
+              key={item.id}
+              src={item.imgSrc}
+              alt={item.altText}
+              title={item.title}
+              text={item.text}
+            />
+          ))}
+        </Grid>
+      </Container>
+    </section>
   );
 }
 
 const styles = {
   grid: {
-    width: ['100%', '80%', '100%'],
-    mx: 'auto',
+    width: ["100%", "80%", "100%"],
+    mx: "auto",
     gridGap: [
-      '35px 0',
+      "35px 0",
       null,
-      '40px 40px',
-      '50px 60px',
-      '30px',
-      '50px 40px',
-      '55px 90px',
+      "40px 40px",
+      "50px 60px",
+      "30px",
+      "50px 40px",
+      "55px 90px"
     ],
     gridTemplateColumns: [
-      'repeat(1,1fr)',
+      "repeat(1,1fr)",
       null,
-      'repeat(2,1fr)',
+      "repeat(2,1fr)",
       null,
-      'repeat(4,1fr)',
-    ],
-  },
+      "repeat(4,1fr)"
+    ]
+  }
 };
