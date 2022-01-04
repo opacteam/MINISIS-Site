11decd1bc3d3662a20f9fdf1d67801a0886b5b79
opacteam <mike@minisisinc.com>
2022-01-04 19:00:50 +0700

    first commit

---
+++ src/components/header/header.data.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    path: "home",
+    label: "Home"
+  },
+  {
+    path: "about",
+    label: "About"
+  },
+  {
+    path: "products",
+    label: "Products"
+  },
+  {
+    path: "services",
+    label: "Services"
+  },
+  {
+    path: "demo",
+    label: "Demo"
+  },
+  {
+    path: "contact",
+    label: "Contacts"
+  }
+];
